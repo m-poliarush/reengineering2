3f8b968222e2d5fb641ee43b57a3fd2a170c93c2
Makysm Poliarush <polarysh2222@gmail.com>
2025-11-13 01:57:24 +0200

    Added tests

---
+++ NetSdrClientAppTests/TcpClientWrapperTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using NetSdrClientApp.Networking;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace NetSdrClientAppTests
+{
+    [TestFixture]
+    public class TcpClientWrapperTests
+    {
+        private TcpListener _testListener;
+        private int _testPort = 54321; 
+        private string _testHost = "127.0.0.1";
+
+        [TearDown]
+        public void TearDown()
+        {
+            _testListener?.Stop();
+        }
+
+        [Test]
+        [TearDown]
+        public async Task Connect_And_Disconnect_ExecutesFullLifecycle()
+        {
+            // Arrange
+            _testListener = new TcpListener(IPAddress.Loopback, _testPort);
+            _testListener.Start();
+
+            var acceptTask = _testListener.AcceptTcpClientAsync();
+            var clientWrapper = new TcpClientWrapper(_testHost, _testPort);
+
+            // Act (Connect)
+            clientWrapper.Connect();
+            TcpClient serverSideClient = await acceptTask;
+            Assert.That(serverSideClient.Connected, Is.True, "Сервер не прийняв клієнта");
+
+            Assert.That(clientWrapper.Connected, Is.True, "Wrapper не вважає себе підключеним");
+
+            // Act (Disconnect)
+            clientWrapper.Disconnect();
+
+            // Assert
+            Assert.That(clientWrapper.Connected, Is.False, "Wrapper не відключився");
+
+            serverSideClient.Close();
+            _testListener.Stop();
+        }
+
+        [Test]
+        public void Disconnect_WhenNotConnected_DoesNotThrow()
+        {
+            // Arrange
+            var clientWrapper = new TcpClientWrapper(_testHost, _testPort);
+
+            Assert.That(clientWrapper.Connected, Is.False);
+
+            // Act & Assert
+            Assert.DoesNotThrow(() =>
+            {
+                clientWrapper.Disconnect();
+            });
+        }
+    }
+}
---
+++ NetSdrClientAppTests/UdpClientWrapperTests.cs
@@ -0,0 +1,136 @@
+﻿using NUnit.Framework;
+using NetSdrClientApp.Networking;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace NetSdrClientAppTests
+{
+    [TestFixture]
+    public class UdpClientWrapperTests
+    {
+        private int _testPort = 54322;
+
+        [Test]
+        public async Task StartListening_And_Exit_ExecutesFullLifecycle()
+        {
+            // Arrange
+            var clientWrapper = new UdpClientWrapper(_testPort);
+
+            // Act
+            
+            var listeningTask = clientWrapper.StartListeningAsync();
+
+            clientWrapper.Exit();
+            await listeningTask;
+
+            // Assert
+            Assert.Pass("StartListeningAsync and Exit completed without exceptions.");
+        }
+
+        [Test]
+        public async Task StartListening_And_Stop_StopsListening()
+        {
+            // Arrange
+            var clientWrapper = new UdpClientWrapper(_testPort);
+
+            // Act
+            var listeningTask = clientWrapper.StartListeningAsync();
+
+            clientWrapper.StopListening();
+            await listeningTask;
+
+            // Assert
+            Assert.Pass("StartListeningAsync and StopListening completed without exceptions.");
+        }
+
+        [Test]
+        public void Exit_WhenNotStarted_DoesNotThrow()
+        {
+            // Arrange
+            var clientWrapper = new UdpClientWrapper(_testPort);
+
+            // Act & Assert
+            Assert.DoesNotThrow(() =>
+            {
+                clientWrapper.Exit();
+            });
+        }
+
+        [Test]
+        public void StopListening_WhenNotStarted_DoesNotThrow()
+        {
+            // Arrange
+            var clientWrapper = new UdpClientWrapper(_testPort);
+
+            // Act & Assert
+            Assert.DoesNotThrow(() =>
+            {
+                clientWrapper.StopListening();
+            });
+        }
+
+        [Test]
+        public async Task MessageReceived_Event_FiresOnMessage()
+        {
+            // Arrange
+            var clientWrapper = new UdpClientWrapper(_testPort);
+            var tcs = new TaskCompletionSource<byte[]>();
+
+            clientWrapper.MessageReceived += (sender, e) =>
+            {
+                tcs.TrySetResult(e);
+            };
+
+            // Act
+            var listeningTask = clientWrapper.StartListeningAsync();
+            using (var sendingClient = new UdpClient())
+            {
+                var testMessage = Encoding.UTF8.GetBytes("test");
+                await sendingClient.SendAsync(testMessage, testMessage.Length, new IPEndPoint(IPAddress.Loopback, _testPort));
+            }
+
+            var receivedMessageTask = tcs.Task;
+            var completedTask = await Task.WhenAny(receivedMessageTask, Task.Delay(1000));
+
+            clientWrapper.Exit();
+            await listeningTask;
+
+            // Assert
+            Assert.That(completedTask, Is.EqualTo(receivedMessageTask), "Повідомлення не було отримано протягом 1 сек.");
+            var receivedMessage = await receivedMessageTask;
+            Assert.That(receivedMessage, Is.EqualTo(Encoding.UTF8.GetBytes("test")));
+        }
+
+        [Test]
+        public void GetHashCode_ReturnsConsistentValue()
+        {
+            // Arrange
+            var clientWrapper1 = new UdpClientWrapper(_testPort);
+            var clientWrapper2 = new UdpClientWrapper(_testPort);
+
+            // Act
+            var hash1 = clientWrapper1.GetHashCode();
+            var hash2 = clientWrapper2.GetHashCode();
+
+            // Assert
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void GetHashCode_ReturnsDifferentValueForDifferentPort()
+        {
+            // Arrange
+            var clientWrapper1 = new UdpClientWrapper(_testPort);
+            var clientWrapper2 = new UdpClientWrapper(_testPort + 1);
+
+            // Act
+            var hash1 = clientWrapper1.GetHashCode();
+            var hash2 = clientWrapper2.GetHashCode();
+
+            // Assert
+            Assert.That(hash1, Is.Not.EqualTo(hash2));
+        }
+    }
+}
